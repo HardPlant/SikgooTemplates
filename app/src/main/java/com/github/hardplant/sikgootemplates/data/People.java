375ab7387a52f938ff975a77a6f93daa1adf5ec2
HardPlant <abc7988se@naver.com>
2018-01-12 08:45:48 +0900

    Initial Comment

---
+++ app/src/main/java/com/github/hardplant/sikgootemplates/data/People.java
@@ -0,0 +1,12 @@
+package com.github.hardplant.sikgootemplates.data;
+
+/**
+ * Created by KANG on 2018-01-11.
+ */
+
+public class People {
+    String Image;
+    String Name;
+    String[] HashTag;
+
+}
---
+++ app/src/main/java/com/github/hardplant/sikgootemplates/data/TimeTable.java
@@ -0,0 +1,8 @@
+package com.github.hardplant.sikgootemplates.data;
+
+/**
+ * Created by KANG on 2018-01-11.
+ */
+
+public class TimeTable {
+}
