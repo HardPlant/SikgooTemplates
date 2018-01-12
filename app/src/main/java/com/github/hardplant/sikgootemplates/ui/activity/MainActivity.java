8d5ed6a29a12eaed9e460d4f82ef5968f1cecb97
HardPlant <abc7988se@naver.com>
2018-01-12 09:19:23 +0900

    toolbar and match button

--- app/src/main/java/com/github/hardplant/sikgootemplates/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/hardplant/sikgootemplates/ui/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.github.hardplant.sikgootemplates.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.github.hardplant.sikgootemplates.R;
 
@@ -11,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void setToolbar(){
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(mToolbar);
     }
 }
