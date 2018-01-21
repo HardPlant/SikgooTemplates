71000a4e46bd3e2c3c581bc69c2856827c0091f7
HardPlant <abc7988se@naver.com>
2018-01-21 11:16:45 +0900

    update

---
+++ app/src/main/java/com/github/hardplant/sikgootemplates/ui/PeopleInviteAdapter.java
@@ -0,0 +1,33 @@
+package com.github.hardplant.sikgootemplates.ui;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by KANG on 2018-01-21.
+ */
+
+class PeopleInviteAdapter extends RecyclerView.Adapter<PeopleInviteAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+            public ViewHolder(View itemView) {
+                super(itemView);
+            }
+    }
+}
