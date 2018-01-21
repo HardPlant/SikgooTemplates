755c1599a894ff475fd05083e3b41e7aa71b488b
HardPlant <abc7988se@naver.com>
2018-01-21 16:07:38 +0900

    cardview

---
+++ app/src/main/java/com/github/hardplant/sikgootemplates/data/Event.java
@@ -0,0 +1,44 @@
+package com.github.hardplant.sikgootemplates.data;
+
+import android.graphics.drawable.Drawable;
+
+import java.util.Date;
+
+/**
+ * Created by KANG on 2018-01-21.
+ */
+
+public class Event {
+    String name;
+    String content;
+    Date startDate;
+    Date endDate;
+    int image;
+
+    public Event(String name, String content, Date startDate, Date endDate) {
+        this.name = name;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/github/hardplant/sikgootemplates/ui/EventAdapter.java
@@ -0,0 +1,65 @@
+package com.github.hardplant.sikgootemplates.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.hardplant.sikgootemplates.R;
+import com.github.hardplant.sikgootemplates.data.Event;
+
+import java.util.List;
+
+/**
+ * Created by KANG on 2018-01-21.
+ */
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+    Context context;
+    List<Event> items;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_event, null);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Event event = items.get(position);
+        Drawable drawable = ContextCompat.getDrawable(context, event.getImage());
+        holder.image.setBackground(drawable);
+        holder.title.setText(event.getName());
+        holder.cardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView title;
+        CardView cardview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.image);
+            title=(TextView) itemView.findViewById(R.id.title);
+            cardview = (CardView) itemView.findViewById(R.id.cardview);
+        }
+    }
+}
