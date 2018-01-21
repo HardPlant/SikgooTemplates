4759165dc1297743b0b47fc9412cad63537cd765
HardPlant <abc7988se@naver.com>
2018-01-21 11:32:52 +0900

    profileActivity

--- app/src/main/java/com/github/hardplant/sikgootemplates/ui/activity/ProfileActivity.java
+++ app/src/main/java/com/github/hardplant/sikgootemplates/ui/activity/ProfileActivity.java
@@ -6,15 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.github.hardplant.sikgootemplates.R;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    ImageButton plusButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        plusButton = (ImageButton) findViewById(R.id.plus_button);
         setToolbar();
     }
 
@@ -35,6 +40,26 @@ public class ProfileActivity extends AppCompatActivity {
         // handle arrow click here
         if (item.getItemId() == android.R.id.home) {
             finish(); // close this activity and return to preview activity (if there is any)
+        }
+        if(item.getItemId() == R.id.plus_button){
+            PopupMenu pm = new PopupMenu(ProfileActivity.this, plusButton);
+            pm.getMenuInflater().inflate(R.menu.menu_profile, pm.getMenu());
+            pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    Toast.makeText(getApplicationContext(), String.valueOf(menuItem.getTitle()), Toast.LENGTH_SHORT).show();
+                    switch(menuItem.getItemId()){
+                        case R.id.invite:
+                            break;
+                        case R.id.call:
+                            break;
+                        default:
+                            break;
+                    }
+                    return true;
+                }
+            });
+            pm.show();
         }
 
         return super.onOptionsItemSelected(item);
